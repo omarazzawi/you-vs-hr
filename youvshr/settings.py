a04725a9d52dce8730cdcbd060fbd2d271a50715
omarazzawi <omar.alazzawi1987@gmail.com>
2025-11-14 22:21:05 +0100

    add CSS custom properties and fade-in animations

--- youvshr/settings.py
+++ youvshr/settings.py
@@ -123,7 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles' # <<< Conect the CSS
+
+STATICFILES_DIRS = [   BASE_DIR / "stories" / "static",
+    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
