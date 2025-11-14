770d6616470c4ee3c20578a4cb9d1847e0bf757f
omarazzawi <omar.alazzawi1987@gmail.com>
2025-11-14 20:48:07 +0100

    Add user authentication and homepage with story grid - fully tested

---
+++ stories/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=False)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
--- stories/views.py
+++ stories/views.py
@@ -1,7 +1,51 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from .forms import RegisterForm
+from .models import Story
 
 def index(request):
-    """Test the web"""
-    return HttpResponse("Welcome to YouVsHR Stories!")
+    
+    stories = Story.objects.all()
+    return render(request, 'stories/index.html', {'stories': stories})
+
+def register(request):
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Account created successfully! Welcome to You VS HR.')
+            return redirect('index')
+    else:
+        form = RegisterForm()
+    return render(request, 'stories/register.html', {'form': form})
+
+def user_login(request):
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome back, {username}!')
+                return redirect('index')
+    else:
+        form = AuthenticationForm()
+    
+   
+    for field in form.fields:
+        form.fields[field].widget.attrs.update({'class': 'form-control'})
+    
+    return render(request, 'stories/login.html', {'form': form})
+
+def user_logout(request):
+    
+    logout(request)
+    messages.success(request, 'Logged out successfully!')
+    return redirect('index')
