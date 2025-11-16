3571f2692fdf2757e98f6307e6f947a7d472f2d5
omarazzawi <omar.alazzawi1987@gmail.com>
2025-11-16 14:23:18 +0100

    implement about page view

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_list_or_404
+from .models import Page
 # Create your views here.
+
+
+def about_page(request):
+    """Display About & Policies page"""
+    page = get_object_or_404(Page, slug='about-policies')
+    return render(request, 'pages/about.html', {'page': page})
