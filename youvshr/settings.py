b411b4090bff4b5084a1ad72bdc77e1d719b63d0
omarazzawi <omar.alazzawi1987@gmail.com>
2025-11-19 13:18:18 +0100

    Resolve static file MIME type error by updating root URL routing

--- youvshr/settings.py
+++ youvshr/settings.py
@@ -143,6 +143,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Connect the CSS
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- youvshr/urls.py
+++ youvshr/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('stories.urls')),
-    path('', include('pages.urls')),
+    path('', include('pages.urls')), 
+    path('stories/', include('stories.urls')), 
 ]
